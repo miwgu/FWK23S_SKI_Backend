b30720dc4123e168963c810fc00beaadcdb63d79
Miwa G <miwa.guhres@hotmail.com>
2024-01-23 11:03:46 +0100

    edit /data, app.use('/data', dataRoutes);

--- src/server.js
+++ src/server.js
@@ -6,6 +6,6 @@ const dataRoutes = require('./routes/data_routes');
 const app = express();
 app.use(cors());  // Use CORS middleware without any restrictions
 app.use(bodyParser.json());
-app.use('/', dataRoutes);
+app.use('/data', dataRoutes);
 
 module.exports = app;
