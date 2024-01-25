73409389e6d6bb87e80666fdc41e53ca08b5fe48
ellinor Jansson lande <ellinor_jansson@hotmail.com>
2024-01-25 13:52:28 +0100

    lagt till mer kommentarer i övrig kod

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
+//installerar express för att kunna använda express.Router()
 const express = require('express');
+//body parser är en middleware som används för att kunna tolka JSON
 const bodyParser = require('body-parser');
+//cors är en middleware som används för att kunna göra cross-origin requests på ett säkert sätt
 const cors = require('cors');
+//importerar routes från data_routes.js
 const dataRoutes = require('./routes/data_routes');
 
 const app = express();
+//använder cors i appen för att kunna göra cross-origin requests på ett säkert sätt
 app.use(cors());
+//använder bodyParser i appen för att kunna tolka JSON
 app.use(bodyParser.json());
+//använder dataRoutes i appen för att kunna använda routes från data_routes.js
 app.use('/data', dataRoutes);
 
 module.exports = app;
--- src/service.js
+++ src/service.js
@@ -1,5 +1,8 @@
+// dotenv config är en middleware som används för att kunna använda .env filen
 require('dotenv').config();
+//const app = require('./server.js') för att kunna använda appen i server.js
 const app = require('./server.js');
+//hämtar port från .env filen
 const PORT = process.env.BACKEND_PORT
 
 app.listen(PORT, () => {
